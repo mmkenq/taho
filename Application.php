e6e0b233dfafd24de7cc1c1b924eb7dbaa8c4d01
Roman <mmkenq@gmail.com>
2023-09-01 21:09:52 +0400

    Hello taho worldcat .gitignore

---
+++ Application.php
@@ -0,0 +1,18 @@
+<?php
+//require_once("./DB.php");
+
+class Application {
+	function __construct(){
+	//	$config = json_decode(file_get_contents('config.json'), true);
+	//	$db = new DB($config['DataBase']);
+
+	//	$this->db = $db;
+	}
+
+	private function signin(){
+	}
+
+	public function taho(){
+	}
+}
+?>
---
+++ router.php
@@ -0,0 +1,61 @@
+<?php
+//header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+require_once("./Application.php");
+
+
+function router($params){
+	$method = $params['method'];
+    if ($method) {
+        $app = new Application();
+        switch ($method) {
+            case 'check' : return true;
+			//case 'getUser': return $app->getUser($params);
+        }
+    }
+    return false;
+}
+
+function answer($data) {
+    if ($data) {
+        return array(
+            'result' => 'ok',
+            'data' => $data
+        );
+    }
+    return array(
+        'result' => 'error'
+    );
+}
+
+
+$path = pathinfo($_SERVER["SCRIPT_FILENAME"]);
+if ($path["extension"] == "js") {
+    header("Content-Type: text/javascript");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+}
+else if ($path["extension"] == "html") {
+    header("Content-Type: text/html");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+}
+else if ($path["extension"] == "css") {
+    header("Content-Type: text/css");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+}
+else if ($path["extension"] == "gif" || $path["extension"] == "jpg") {
+    header("Content-Type: image/gif");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+}
+else {
+	if (preg_match('/api/', $_SERVER["REQUEST_URI"])){
+		header("Content-Type: application/json");
+		echo(json_encode(answer(router($_GET))));
+	} 
+	else{
+		include_once './public_html/index.html';
+	}
+    //return FALSE;
+}
+
+?>
+
